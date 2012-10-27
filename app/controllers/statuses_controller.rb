55cc374f75a48b19f16dc46da40fad52663b6581
Renato Carvalho <renatolz@gmail.com>
2012-10-26 23:05:57 -0200

    ordenando posts por data

--- app/controllers/statuses_controller.rb
+++ app/controllers/statuses_controller.rb
@@ -2,7 +2,7 @@ class StatusesController < ApplicationController
   # GET /statuses
   # GET /statuses.json
   def index
-    @statuses = Status.all
+    @statuses = Status.order('created_at DESC').all
 
     respond_to do |format|
       format.html # index.html.erb
