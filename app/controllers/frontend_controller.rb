5397faa213ec872c8b4b8434e496a01be68a28d7
Renato Carvalho <renatolz@gmail.com>
2012-10-29 23:01:24 -0200

    Adicionando rotas estaticas na pasta frontend.

---
+++ app/controllers/frontend_controller.rb
@@ -0,0 +1,8 @@
+class FrontendController < ApplicationController
+  def index
+  end
+
+  def show
+    render params[:template]
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Fakebook::Application.routes.draw do
   resources :statuses
+  
+  match 'frontend' => 'frontend#index'
+  match 'frontend/:template' => 'frontend#show'
 
   root to: 'statuses#index'
 end
